f7715ad1a68b528c44e4b0a39dfa13966a5f90d9
Randy Lopez <logentix@gmail.com>
2023-12-16 22:13:13 -0500

    cleaned up code a bit

--- firmware/CAN_Shark.ino
+++ firmware/CAN_Shark.ino
@@ -1,70 +1,4 @@
-// #include "src/Sparkfun_Shield_Custom/Canbus.h"
-// #include "src/Sparkfun_Shield_Custom/defaults.h"
-// #include "src/Sparkfun_Shield_Custom/global.h"
-// #include "src/Sparkfun_Shield_Custom/mcp2515.h"
-// #include "src/Sparkfun_Shield_Custom/mcp2515_defs.h"
-// #include <ArduinoJson.h>
-
-// int data;
-// String message;
-
-// void setup()
-// {
-//   Serial.begin(115200);
-
-//   if (Canbus.init(CANSPEED_500)) /* Initialise MCP2515 CAN controller at the specified speed */
-//   {
-//     Serial.println("{\"status\": \"[ OK ]\"}");
-//   }
-//   else
-//   {
-//     Serial.println("{\"status\": \"[ ERROR ] Unable to initialize MCP2515 CAN controller\"}");
-//   }
-
-//   delay(1000);
-// }
-
-// void loop()
-// {
-//   while (Serial.available()) // only send data back if data has been received
-//   {
-//     char data = Serial.read(); // read incoming bytes
-//     message += String(data);   // concat incoming bytes to message string
-//     if (String(data) == "}") // find end delimiter
-//     {
-//       StaticJsonDocument<456> doc;                                // will hold json data
-//       DeserializationError error = deserializeJson(doc, message); // deserialize json data
-//       delay(30); // rest
-//       int i;
-//       String response = "[";
-//       //Serial.println(message);
-//       for (i = 0; i < sizeof(doc["pids"]); i++)
-//       {
-//         String out = Canbus.ecu_req(doc["pids"][i]); // read message from can bus
-//         if (out == NULL || out == "" || out == " ")
-//         {
-//           response += "\"___\",";
-//           Serial.print(out);
-//         }
-//         else
-//         {
-//           response += out;
-//         }
-//         delay(30); // rest
-//       }
-//       response.remove(response.length() - 1); // remove trailing commas for parser
-//       response += "]";
-//       Serial.println(response); // send message
-//       delay(30);                // allow buffer to fill
-//       message = "";             // clear message
-//     }
-//     Serial.flush(); // move on when serial is clear
-//   }
-// }
-
 /************************************************************************************************* 
-  OBD-II_PIDs TEST CODE
-  LOOVEE @ JUN24, 2017
   
   Query
   send id: 0x7df
@@ -130,11 +64,11 @@ void task_can()
     unsigned char len = 0;
     unsigned char buf[8];
 
-    if (CAN_MSGAVAIL == CAN.checkReceive()) // check if get data
+    if (CAN_MSGAVAIL == CAN.checkReceive()) // check if we received a message
     {
         CAN.readMsgBuf(&len, buf); // read data,  len: data length, buf: data buf
 
-        Serial.println("\r\n------------------------------------------------------------------");
+        Serial.println("\r\n---------------------------------------------------");
         Serial.print("Get Data From id: 0x");
         Serial.println(CAN.getCanId(), HEX);
         for (int i = 0; i < len; i++) // print the data
@@ -179,13 +113,11 @@ void task_serial()
 void setup()
 {
     Serial.begin(115200);
-    while (CAN_OK != CAN.begin(CAN_500KBPS)) // init can bus : baudrate = 500k
+    while (CAN_OK != CAN.begin(CAN_500KBPS)) // wait for CAN bus init with baudrate = 500k
     {
-        //Serial.println("CAN BUS Shield init fail");
-        //Serial.println(" Init CAN BUS Shield again");
         delay(100);
     }
-    //Serial.println("CAN BUS Shield init ok!");
+    Serial.println("CAN BUS Shield init ok!");
     set_mask_filt();
 }
 
